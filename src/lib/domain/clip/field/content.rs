15b4dc3eef6751e5c97a21ea72cf92b14418d816
r1oga <38692952+r1oga@users.noreply.github.com>
2022-09-01 16:09:07 +0200

    feat: implement Content

--- src/lib/domain/clip/field/content.rs
+++ src/lib/domain/clip/field/content.rs
@@ -1,1 +1,24 @@
-use super::Cl
+use crate::domain::clip::ClipError;
+// use super::ClipError;
+
+#[derive(Clone, Debug, Deserialize, Serialize)] // needs for serde because of the JSON API
+pub struct Content(String);
+
+impl Content {
+    pub fn new(content: &str) -> Result<Self, ClipError> {
+        if !content.trim().is_empty() {
+            Ok(Self(content.to_owned()))
+        } else {
+            Err(ClipError::EmptyContent)
+        }
+    }
+
+    // moving self and return inner value
+    pub fn into_inner(self) -> String {
+        self.0
+    }
+
+    pub fn as_str(&self) -> &str {
+        self.0.as_str()
+    }
+}
