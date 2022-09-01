9cfbdc99c8688a3ba28f0f2c2886b41a9290b099
r1oga <38692952+r1oga@users.noreply.github.com>
2022-09-01 16:00:41 +0200

    feat: define Clip errors

--- src/lib/domain/clip/mod.rs
+++ src/lib/domain/clip/mod.rs
@@ -1,5 +1,31 @@
 pub mod field;
 
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum ClipError {
+    #[error("invalid password: {0}")]
+    InvalidPassword(String),
+
+    #[error("invalid title: {0}")]
+    InvalidTitle(String),
+
+    #[error("empty content")]
+    EmptyContent,
+
+    #[error("invalid date: {0}")]
+    InvalidDate(String),
+
+    #[error("date parse error: {0}")]
+    DateParse(#[from] chrono::ParseError),
+
+    #[error("id parse error: {0}")]
+    Id(#[from] uuid::Error),
+
+    #[error("hits parse error: {0}")]
+    Hits(#[from] std::num::TryFromIntError),
+}
+
 #[derive(Debug, CLone, Deserialize, Serialize)]
 pub struct Clip {
     pub clip_id: field::ClipId,
