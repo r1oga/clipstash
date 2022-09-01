cd12e417f96c6bcd98013a35767c434a7379ed6d
r1oga <38692952+r1oga@users.noreply.github.com>
2022-09-01 17:05:27 +0200

    feat: implement DbId

---
+++ src/lib/data/mod.rs
@@ -0,0 +1,29 @@
+use derive_more::{Display, From};
+use uuid::Uuid;
+use std::str::FromStr;
+
+#[derive(Clone, Debug, Display, From, Deserialize, Serialize)]
+pub struct DbId(Uuid);
+
+impl DbId {
+    pub fn new() -> Self {
+        Uuid::new_v4().into()
+    }
+
+    pub fn nil() -> Self {
+        Self(Uuid::nil())
+    }
+}
+
+impl Default for DbId {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl FromStr for DbId {
+    type Err = uuid::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(DbId(Uuid::parse_str(s)?))
+    }
+}
