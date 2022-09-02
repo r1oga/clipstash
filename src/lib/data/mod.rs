a6f61dc7d64ca276d9b3befff40115b215e8f9a9
r1oga <38692952+r1oga@users.noreply.github.com>
2022-09-02 19:11:20 +0200

    feat: implement data module (#2)
    * feat: define Database types and struct (pool, tx, row...)
    
    * feat: impement Clip data model
    
    * refactor: restrict pub access to Clip data to `data` module
    
    * fix initial migration sql file
    
    * feat: get clip, new clip
    
    * feat: update clip

--- src/lib/data/mod.rs
+++ src/lib/data/mod.rs
@@ -1,7 +1,44 @@
+pub mod model;
+pub mod query;
+
 use serde::{Deserialize, Serialize};
 use derive_more::{Display, From};
 use uuid::Uuid;
 use std::str::FromStr;
+use sqlx::{Sqlite};
+
+#[derive(Debug, thiserror::Error)]
+pub enum DataError {
+    #[error("database error: {0}")]
+    Database(#[from] sqlx::Error)
+}
+
+// encapsulating in custom type
+// so that we have better control
+// and that changes to make are collocated here
+// in case there is a crate update or we decide to change DB type
+pub type Db = Database<Sqlite>;
+pub type DbPool = sqlx::sqlite::SqlitePool; // pool of connections. Reuse already open connections for better perf
+pub type Tx<'t> = sqlx::Transaction<'t, Sqlite>; // to allow rolling back
+pub type DbRow = sqlx::sqlite::SqliteRow;
+pub type QueryResult = sqlx::sqlite::SqliteQueryResult;
+
+pub struct Database<D: sqlx::Database>(sqlx::Pool<D>);
+impl Database<Sqlite> {
+  pub async fn new(db_url:&str) -> Self {
+      let pool = sqlx::sqlite::SqlitePoolOptions::new().connect(db_url).await;
+      match pool {
+          Ok(pool) => Self(pool),
+          Err(e) => {
+              eprintln!("{:?}\n", e);
+              eprintln!("If the database has not yet been created, run:\n$sqlx database setup");
+              panic!("database connection error")
+          }
+      }
+  }
+
+    pub fn get_pool(&self) -> &DbPool{ &self.0 }
+}
 
 #[derive(Clone, Debug, Display, From, Deserialize, Serialize)]
 pub struct DbId(Uuid);
---
+++ src/lib/data/model.rs
@@ -0,0 +1,77 @@
+use crate::data::DbId;
+use crate::{ClipError, ShortCode, Time};
+use chrono::{NaiveDateTime, Utc};
+use std::convert::TryFrom;
+
+// DB friendly types
+#[derive(Debug, sqlx::FromRow)]
+pub struct Clip {
+    pub(in crate::data) clip_id: String,
+    pub(in crate::data) shortcode: String,
+    pub(in crate::data) content: String,
+    pub(in crate::data) title: Option<String>,
+    pub(in crate::data) posted: NaiveDateTime,
+    pub(in crate::data) expires: NaiveDateTime,
+    pub(in crate::data) password: Option<String>,
+    pub(in crate::data) hits: i64,
+}
+
+impl TryFrom<Clip> for crate::domain::clip::Clip {
+    type Error = ClipError;
+
+    fn try_from(clip: Clip) -> Result<Self, Self::Error> {
+        use crate::domain::clip::field::*;
+        use std::str::FromStr;
+
+        Ok(Self {
+            clip_id: ClipId::new(DbId::from_str(clip.clip_id.as_str())?),
+            shortcode: ShortCode::from(clip.shortcode.as_str()),
+            content: Content::new(clip.content.as_str())?,
+            title: Title::new(clip.title),
+            posted: Posted::new(Time::from_naive_utc(clip.posted)),
+            expires: Expires::new(Time::from_naive_utc(clip.expires)),
+            password: Password::new(clip.password.unwrap_or_default())?,
+            hits: Hits::new(u64::try_from(clip.hits)?),
+        })
+    }
+}
+
+pub struct GetClip {
+    pub(in crate::data) shortcode: String,
+}
+
+impl From<ShortCode> for GetClip {
+    fn from(shortcode: ShortCode) -> Self {
+        GetClip {
+            shortcode: shortcode.into_inner()
+        }
+    }
+}
+
+impl From<String> for GetClip {
+    fn from(shortcode: String) -> Self {
+        GetClip {
+            shortcode
+        }
+    }
+}
+
+pub struct NewClip {
+    pub(in crate::data) clip_id: String,
+    pub(in crate::data) shortcode: String,
+    pub(in crate::data) content: String,
+    pub(in crate::data) title: Option<String>,
+    pub(in crate::data) posted: i16,
+    pub(in crate::data) expires: Option<NaiveDateTime>,
+    pub(in crate::data) password: Option<String>,
+    pub(in crate::data) hits: i64,
+}
+
+
+pub struct UpdateClip {
+    pub(in crate::data) shortcode: String,
+    pub(in crate::data) content: String,
+    pub(in crate::data) title: Option<String>,
+    pub(in crate::data) expires: Option<i64>,
+    pub(in crate::data) password: Option<String>,
+}
