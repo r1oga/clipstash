5be685712100e8c91862bdae46c98d6b13237c62
r1oga <38692952+r1oga@users.noreply.github.com>
2022-09-14 22:53:09 +0200

    define `PageError`

--- src/lib/web/mod.rs
+++ src/lib/web/mod.rs
@@ -1,2 +1,29 @@
+use handlebars::RenderError;
+
 pub mod ctx;
 pub mod renderer;
+
+#[derive(rocket::Responder)]
+pub enum PageError {
+    #[response(status = 500)]
+    Serialization(String),
+    #[response(status = 500)]
+    Render(String),
+    #[response(status = 404)]
+    NotFound(String),
+    #[response(status = 500)]
+    Internal(String),
+}
+
+// thiserror::Error not compatible with Responder, need to impl From manually
+impl From<RenderError> for PageError {
+    fn from(err: RenderError) -> Self {
+        PageError::Render(format!("{}", err))
+    }
+}
+
+impl From<serde_json::Error> for PageError {
+    fn from(err: serde_json::Error) -> Self {
+        PageError::Serialization(format!("{}", err))
+    }
+}
