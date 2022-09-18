42ddc529dbb4070fc9b295e8fdb3bfaad072d00a
r1oga <38692952+r1oga@users.noreply.github.com>
2022-09-18 17:37:50 +0200

    feat: define api router

--- src/lib/web/api.rs
+++ src/lib/web/api.rs
@@ -7,7 +7,7 @@ use serde::Serialize;
 use std::str::FromStr;
 
 use crate::data::Db;
-use crate::service;
+use crate::{service, ShortCode};
 use crate::service::action;
 use crate::web::{HitCounter, PASSWORD_COOKIE};
 use crate::ServiceError;
@@ -77,5 +77,143 @@ impl From<ServiceError> for ApiError {
             ServiceError::Data(_) => Self::Server(Json("a server error occurred".to_owned())),
             ServiceError::PermissionError(msg) => Self::User(Json(msg))
         }
+    }
+}
+
+// to use api key as rocket request guard
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for ApiKey {
+    type Error = ApiError;
+
+    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        fn server_error() -> Outcome<ApiKey, ApiError> {
+            Outcome::Failure((
+                Status::InternalServerError,
+                ApiError::Server(Json("server error".to_owned()))
+            ))
+        }
+
+        fn key_error(e: ApiKeyError) -> Outcome<ApiKey, ApiError> {
+            Outcome::Failure((
+                Status::BadRequest,
+                ApiError::KeyError(Json(e))
+            ))
+        }
+
+        match request.headers().get_one(API_KEY_HEADER) {
+            None => key_error(ApiKeyError::NotFound("Api key not found".to_owned())),
+            Some(key) => {
+                let db = match request.guard::<&State<Db>>().await {
+                    Outcome::Success(db) => db,
+                    _ => return server_error()
+                };
+
+                let api_key = match ApiKey::from_str(key) {
+                    Ok(key) => key,
+                    Err(e) => return key_error(e)
+                };
+
+                match action::api_key_is_valid(api_key.clone(), db.get_pool()).await {
+                    Ok(valid) if valid => Outcome::Success(api_key),
+                    Ok(valid) if !valid => {
+                        key_error(ApiKeyError::NotFound("API key is not found".to_owned()))
+                    },
+                    _ => server_error()
+                }
+            }
+        }
+    }
+}
+
+#[rocket::get("/key")]
+pub async fn new_api_key(db:&State<Db>) -> Result<Json<&str>, ApiError> {
+    let api_key = action::new_api_key(db.get_pool()).await?;
+    println!("Api Key: {:?}", api_key.to_base64());
+    Ok(Json("Api key created. See logs for details"))
+}
+
+#[rocket::get("/<shortcode>")]
+pub async fn get_clip(
+    shortcode: &str,
+    db: &State<Db>,
+    cookies: &CookieJar<'_>,
+    hit_counter: &State<HitCounter>,
+    _api_key: ApiKey, // _ as prefix to tell compiler to ignore it if not used
+) -> Result<Json<crate::Clip>, ApiError> {
+    use crate::domain::clip::field::Password;
+
+    let req = service::ask::GetClip {
+        shortcode: shortcode.into(),
+        password: cookies
+            .get(PASSWORD_COOKIE)
+            .map(|cookie|cookie.value())
+            .map(|raw_pwd|Password::from_str(raw_pwd).ok())
+            .flatten()
+            .unwrap_or_else(Password::default)
+    };
+
+    let clip = action::get_clip(req, db.get_pool()).await?;
+    hit_counter.hit(shortcode.into(), 1);
+    Ok(Json(clip))
+}
+
+#[rocket::post("/", data = "<req>")]
+pub async fn new_clip(
+    req: Json<service::ask::NewClip>,
+    db: &State<Db>,
+    _api_key: ApiKey
+) -> Result<Json<crate::Clip>, ApiError> {
+    let clip = action::new_clip(req.into_inner(), db.get_pool()).await?;
+    Ok(Json(clip))
+}
+
+#[rocket::put("/", data = "<req>")]
+pub async fn update_clip(
+    req: Json<service::ask::UpdateClip>,
+    db: &State<Db>,
+    _api_key: ApiKey
+) -> Result<Json<crate::Clip>, ApiError> {
+    let clip = action::update_clip(req.into_inner(), db.get_pool()).await?;
+    Ok(Json(clip))
+}
+
+pub fn routes() -> Vec<rocket::Route> {
+    rocket::routes!(get_clip, new_clip, update_clip)
+}
+
+pub mod catcher {
+    use rocket::serde::json::Json;
+    use rocket::Request;
+    use rocket::{catch, catchers, Catcher};
+
+    #[catch(default)]
+    fn default(req: &Request) -> Json<&'static str> {
+        eprintln!("General error: {:?}", req);
+        Json("Something went wrong")
+    }
+
+    #[catch(500)]
+    fn internal_error(req: &Request) -> Json<&'static str> {
+        eprintln!("Internal error: {:?}", req);
+        Json("internal server error")
+    }
+
+    #[catch(404)]
+    fn not_found() -> Json<&'static str> {
+        Json("404")
+    }
+
+    #[catch(401)]
+    fn request_error() -> Json<&'static str> {
+        Json("request error")
+    }
+
+    #[catch(400)]
+    fn missing_api_key() -> Json<&'static str> {
+        Json("Api key missing or invalid")
+    }
+
+    pub fn catchers() -> Vec<Catcher> {
+        catchers![not_found, default, internal_error, not_found, request_error]
     }
 }
