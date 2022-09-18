39932e72a10f40832e3c0dee8352032650de5565
r1oga <38692952+r1oga@users.noreply.github.com>
2022-09-18 12:51:53 +0200

    chore: format

--- src/lib/data/query.rs
+++ src/lib/data/query.rs
@@ -9,7 +9,13 @@ type Result<T> = std::result::Result<T, DataError>;
 pub async fn get_clip<M: Into<GetClip>>(model: M, pool: &DbPool) -> Result<model::Clip> {
     let model = model.into();
     let shortcode = model.shortcode.as_str();
-    Ok(sqlx::query_as!(model::Clip, "SELECT * FROM clips WHERE shortcode = ?", shortcode).fetch_one(pool).await?)
+    Ok(sqlx::query_as!(
+        model::Clip,
+        "SELECT * FROM clips WHERE shortcode = ?",
+        shortcode
+      )
+      .fetch_one(pool)
+      .await?)
 }
 
 // using a model as param to avoid having to pass a whole bunch of clip properties as params
