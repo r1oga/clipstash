d03fb6f0627f6622edb96fe5d4e04486d0e0d950
r1oga <38692952+r1oga@users.noreply.github.com>
2022-09-18 22:06:52 +0200

    test http requests

--- src/lib/web/mod.rs
+++ src/lib/web/mod.rs
@@ -5,7 +5,9 @@ pub mod renderer;
 pub mod form;
 pub mod http;
 pub mod hit_counter;
+
 pub use hit_counter::HitCounter;
+
 pub mod api;
 
 pub const PASSWORD_COOKIE: &str = "password";
@@ -32,5 +34,36 @@ impl From<RenderError> for PageError {
 impl From<serde_json::Error> for PageError {
     fn from(err: serde_json::Error) -> Self {
         PageError::Serialization(format!("{}", err))
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use crate::test::async_runtime;
+    use crate::{Renderer, RocketConfig};
+    use rocket::local::blocking::Client;
+
+    pub fn config() -> RocketConfig {
+        use crate::web::{hit_counter::HitCounter, renderer::Renderer};
+
+        let rt = async_runtime();
+        let renderer = Renderer::new("templates/".into());
+        let db = crate::data::test::new_db(rt.handle());
+        let maintenance = crate::domain::maintenance::Maintenance::spawn(
+            db.get_pool().clone(),
+            rt.handle().clone());
+        let hit_counter = HitCounter::new(db.get_pool().clone(), rt.handle().clone());
+
+        RocketConfig {
+            renderer,
+            db,
+            hit_counter,
+            maintenance
+        }
+    }
+
+    pub fn client() -> Client {
+        let config = config();
+        Client::tracked(crate::rocket(config)).expect("failed to build rocket instance")
     }
 }
