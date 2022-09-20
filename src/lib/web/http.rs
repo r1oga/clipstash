8a15c98bb6314747dbae0558aebf9aa4841eaeac
r1oga <38692952+r1oga@users.noreply.github.com>
2022-09-20 09:22:22 +0200

    test sqlite queries and http requests (#10)
    * test new and get clip queries
    
    * test http requests
    
    * fix: rename `add_clip` route to `new_clip`
    
    * fix: `Db` route instead of `AppDatabase`

--- src/lib/web/http.rs
+++ src/lib/web/http.rs
@@ -21,7 +21,7 @@ fn home(renderer: &State<Renderer<'_>>) -> RawHtml<String> {
 
 
 #[rocket::post("/", data = "<form>")]
-pub async fn add_clip(
+pub async fn new_clip(
     // rocket can only call a function for a route if all data exists.
     // form data may not exist or be incorrect
     // using Contextual allows to accept invalid form data
@@ -190,7 +190,7 @@ pub fn routes() -> Vec<rocket::Route> {
     rocket::routes![
         home,
         get_clip,
-        add_clip,
+        new_clip,
         submit_clip_password,
         get_raw_clip
     ]
@@ -222,4 +222,80 @@ pub mod catcher {
     }
 }
 
+#[cfg(test)]
+pub mod test {
+    use crate::data::Db;
+    use crate::test::async_runtime;
+    use crate::web::test::client;
+    use rocket::http::Status;
+    use serde_json::Value::String;
+
+    #[test]
+    fn gets_home() {
+        let client = client();
+        let res = client.get("/").dispatch();
+        assert_eq!(res.status(), Status::Ok);
+    }
+
+    #[test]
+    fn not_found__error_on_unknown_clip_shortcode() {
+        let client = client();
+        let res = client.get("/clip/foo").dispatch();
+        assert_eq!(res.status(), Status::NotFound);
+    }
+
+    #[test]
+    fn requires_pwd_if_defined() {
+        use crate::domain::clip::field::{Content, Expires, Password, Title};
+        use crate::service;
+        use rocket::http::{ContentType, Cookie};
+
+        let rt = async_runtime();
+
+        let client = client();
+        let db = client.rocket().state::<Db>().unwrap();
+
+        let req = service::ask::NewClip {
+            content: Content::new("content").unwrap(),
+            expires: Expires::default(),
+            password: Password::new("123".to_owned()).unwrap(),
+            title: Title::default(),
+        };
+        let clip = rt
+            .block_on(async move { service::action::new_clip(req, db.get_pool()).await })
+            .unwrap();
+        let response = client
+            .get(format!("/clip/{}", clip.shortcode.as_str()))
+            .dispatch();
+
+        // Unauthorized error when no password is provided
+        assert_eq!(response.status(), Status::Unauthorized);
+
+        let response = client
+            .get(format!("/clip/raw/{}", clip.shortcode.as_str()))
+            .dispatch();
+        assert_eq!(response.status(), Status::Unauthorized);
+
+        // Get clip when the password is provided
+        let response = client
+            .post(format!("/clip/{}", clip.shortcode.as_str()))
+            .header(ContentType::Form)
+            .body("password=123")
+            .dispatch();
+        assert_eq!(response.status(), Status::Ok);
+
+        let response = client
+            .get(format!("/clip/raw/{}", clip.shortcode.as_str()))
+            .cookie(Cookie::new("password", "123"))
+            .dispatch();
+        assert_eq!(response.status(), Status::Ok);
+
+        // Get clip when the password is provided, but incorrect
+        let response = client
+            .get(format!("/clip/raw/{}", clip.shortcode.as_str()))
+            .cookie(Cookie::new("password", "abc"))
+            .dispatch();
+        assert_eq!(response.status(), Status::Unauthorized);
+    }
+}
 
